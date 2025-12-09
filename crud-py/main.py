4acf113647dcdbdefd698df83fd90f96ce09e142
Мацегора Дмитрий Романович <drmatsegor@mts.ru>
2025-12-09 07:49:50 +0300

    fix

--- crud-py/main.py
+++ crud-py/main.py
@@ -43,4 +43,4 @@ app.include_router(create_user_router(user_interactor))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
